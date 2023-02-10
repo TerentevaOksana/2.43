e0f3a0b662c83b09a720b953dc96f8fcf8b22e8b
TerentevaOksana <113889184+TerentevaOksana@users.noreply.github.com>
2023-02-10 12:15:56 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class program
+{
+    static void Main(string[] args)
+    {
+        int a, b, x;
+        Console.WriteLine("Введите значение a");
+        a = int.Parse(Console.ReadLine());
+        Console.WriteLine("Введите значение b");
+        b = int.Parse(Console.ReadLine());
+        x = ((a % b) * (b % a) + 1);
+        Console.WriteLine(x);
+        Console.ReadLine();
+
+    }
+}
